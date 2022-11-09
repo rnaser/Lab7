53da86ebfd88395fe729503337dafdaa194eb674
rnaser <111962217+rnaser@users.noreply.github.com>
2022-11-09 03:34:55 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,25 +3,16 @@ function init(){
  
   function myEventFunction(){
     alert( "Rami Naser: " + inn.value));
+	var inn = document.getElementById('entryinput');		
+	var ban = document.getElementById('textoutput');		
+	ban.innerHTML = inn;		
 			
   }
  
-  var inn = document.getElementById('entryinput');
+  
   var btn = document.getElementById('entrybutton');
-  var ban = document.getElementById('textoutput');
+ 
   btn.addEventListener('click',myEventFunction);
-  btn.addEventListener('click',function(){
-	 
-    ban.innerHTML = ('<h2 id="textoutput" class="center">(inn.value)</h2>');
-  }};
- 
- 
-     
-		
-     
-		
-		
-
-
+  
 }
 window.addEventListener('load', init);
