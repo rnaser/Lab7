a328b472f72ff9c242886669127b6fb8c5cff309
rnaser <111962217+rnaser@users.noreply.github.com>
2022-11-09 00:32:44 -0600

    fixed small things

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 function init(){
 //add your javascrip between these two lines of code
  
-
+   function myEventFunction(){
+			alert( id=entryinput);
+		}
+ 
+   entryinput.addEventListener('click',myEventFunction);
+ 
+ 
+var fc = document.getElementById('textcontrol')
+		//use fc.value in inspector to see the input value
+		alert(fc.value);
+		fc.value = "javascript worked";	
 
 
 
