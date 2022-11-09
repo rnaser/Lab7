c28411d4b2850726e31bf01d781ad2e61dc50c78
rnaser <111962217+rnaser@users.noreply.github.com>
2022-11-09 03:08:59 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,15 +2,17 @@ function init(){
 //add your javascrip between these two lines of code
  
    function myEventFunction(){
-			alert( "Rami Naser: " + inn.value );
+			alert( "Rami Naser: " + inn.value 
+			ban.innerHTML = '<h2 id="textoutput" class="center">inn.value</h2>';
 			
 			
 		}
  
        var inn = document.getElementById('entryinput');
 	   var btn = document.getElementById('entrybutton');
-	   var ban = document.getElementById('entrybutton');
+	   var ban = document.getElementById('textoutput');
 	   btn.addEventListener('click',myEventFunction);
+	   ban.innerHTML = <h2 id="textoutput" class="center">inn.value</h2>
  
  
      
