eb5707754587f94e976ea063338b369acf2cf218
rnaser <111962217+rnaser@users.noreply.github.com>
2022-11-09 03:07:35 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,21 +2,20 @@ function init(){
 //add your javascrip between these two lines of code
  
    function myEventFunction(){
-			alert( "Rami Naser:" + inn.value );
+			alert( "Rami Naser: " + inn.value );
+			
 			
 		}
  
        var inn = document.getElementById('entryinput');
-	   
-		//use fc.value in inspector to see the input value
-		inn.addEventListener('click',myEventFunction);
-		
-			
+	   var btn = document.getElementById('entrybutton');
+	   var ban = document.getElementById('entrybutton');
+	   btn.addEventListener('click',myEventFunction);
  
  
-     var btn = document.getElementById('entrybutton');
+     
 		
-     btn.addEventListener('click',myEventFunction);
+     
 		
 		
 
