93c1546ba9d23da863342a207503b454e80ed011
rnaser <111962217+rnaser@users.noreply.github.com>
2022-11-09 02:45:15 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,15 +2,16 @@ function init(){
 //add your javascrip between these two lines of code
  
    function myEventFunction(){
-			alert( "Rami Naser:" );
-			//out.innerHTML = '<h2 id="textoutput" class="center">working</h2>'
+			alert( "Rami Naser:" + inn );
+			
 		}
  
-      /* var inn = document.getElementById('entryinput');
+       var inn = document.getElementById('entryinput');
+	   
 		//use fc.value in inspector to see the input value
 		inn.addEventListener('click',myEventFunction);
 		alert(fc.value);
-		fc.value = "javascript worked";	*/
+		fc.value = "javascript worked";	
  
  
      var btn = document.getElementById('entrybutton');
@@ -18,14 +19,6 @@ function init(){
      btn.addEventListener('click',myEventFunction);
 		
 		
-		
-		
-		/*   alert(btn.value);
-		btn.value = "javascript worked";	*/
-
-
-
-var out = document.getElementById('textoutput');
 
 
 }
