16e04ce6e6713a6e8ce2031ad5c93acfa165b344
rnaser <111962217+rnaser@users.noreply.github.com>
2022-11-09 03:23:09 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function init(){
 //add your javascrip between these two lines of code
  
    function myEventFunction(){
-			alert( "Rami Naser: " + inn.value);
+			alert( "Rami Naser: " + inn.value));
 			
 			
 			
@@ -14,7 +14,7 @@ function init(){
 	   btn.addEventListener('click',myEventFunction);
 	   btn.addEventListener('click',function(){
 	 
-	     ban.innerHTML = '<h2 id="textoutput" class="center">inn.value</h2>';
+	     ban.innerHTML = ('<h2 id="textoutput" class="center">inn.value</h2>');
        }};
  
  
