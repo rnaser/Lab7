5a3ab4c17b19c1a0bf882e91fca6c9f8a93047bc
rnaser <111962217+rnaser@users.noreply.github.com>
2022-11-09 03:11:26 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function init(){
 //add your javascrip between these two lines of code
  
    function myEventFunction(){
-			alert( "Rami Naser: " + inn.value 
+			alert( "Rami Naser: " + inn.value)
 			ban.innerHTML = '<h2 id="textoutput" class="center">inn.value</h2>';
 			
 			
