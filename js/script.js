cf8bc576d4579f7c3f655b24cbbe8a084fb460b4
rnaser <111962217+rnaser@users.noreply.github.com>
2022-11-09 00:53:42 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,20 +2,26 @@ function init(){
 //add your javascrip between these two lines of code
  
    function myEventFunction(){
-			alert( "hello");
+			alert( "Rami Naser:" + inn);
+			out.innerHTML = '<h2 id="textoutput" class="center">working</h2>'
 		}
  
-   entrybutton.addEventListener('click',myEventFunction);
- 
- 
-var fc = document.getElementById('entrybutton')
+  /* var inn = document.getElementById('entryinput');
 		//use fc.value in inspector to see the input value
-		fc.addEventListener('click',myEventFunction);
+		inn.addEventListener('click',myEventFunction);
 		alert(fc.value);
-		fc.value = "javascript worked";	
+		fc.value = "javascript worked";	*/
+ 
+ 
+var btn = document.getElementById('entrybutton')
+		//use fc.value in inspector to see the input value
+		btn.addEventListener('click',myEventFunction);
+		alert(btn.value);
+		btn.value = "javascript worked";	
 
 
 
+var out = document.getElementById('textoutput');
 
 
 
